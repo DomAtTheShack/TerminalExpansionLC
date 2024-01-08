f199daebe6c63e7a89624bc3ff5621f5f5223303
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-01-08 13:46:22 -0500

    Removed Defaulft Code

--- TerminalExpansion/Plugin.cs
+++ TerminalExpansion/Plugin.cs
@@ -40,67 +40,14 @@ namespace TerminalExpansion
                 Category = "Other"
             });
 
-			// Will display 'World' when 'hello' is typed into the terminal
-			AddCommand("hello", "World\n");
-
-			// Will display 'Sorry but you cannot run kill' when 'run kill' is typed into the terminal
-			// Will also display the same thing as above if you just type 'kill' into the terminal 
-			// because the default verb will be 'run'
-			AddCommand("kill", "Sorry but you cannot run kill\n", "run");
-
-			// All the code below is essentially the same as the line of code above
-            TerminalNode triggerNode = CreateTerminalNode($"Frank is not available right now.\n", true);
-            TerminalKeyword verbKeyword = CreateTerminalKeyword("get", true);
-            TerminalKeyword nounKeyword = CreateTerminalKeyword("frank");
-
-            verbKeyword = verbKeyword.AddCompatibleNoun(nounKeyword, triggerNode);
-            nounKeyword.defaultVerb = verbKeyword;
-
-            AddTerminalKeyword(verbKeyword);
-
-			// The second parameter passed in is a CommandInfo, if you want to have a callback.
-            AddTerminalKeyword(nounKeyword, new() { 
-				TriggerNode = triggerNode,
-				DisplayTextSupplier = () =>
-				{
-					Logger.LogWarning("Put code here, and it will run when trigger node is loaded");
-					return "This text will display";
-				},
-				Category = "Other",
-				Description = "This is just a test command."
-				// The above would look like '>FRANK\nThis is just a test command.' in Other
-			});
-
-			// Adds a new command/terminal keyword that is 'pop' and a callback function that will run when the node of the keyword is loaded
-			AddCommand("pop", new CommandInfo()
-			{
-				DisplayTextSupplier = () =>
-				{
-					Logger.LogWarning("Wowow, this ran.");
-					return "popped\n\n";
-				},
-				Category = "Other"
-			});
-
-			// Or
-
-			AddCommand("push", new CommandInfo()
-			{
-				DisplayTextSupplier = CommandFunction,
-				Category = "Misc" // Does not work for categories that do not exist, yet ;)
-			});
+			
         }
 
 		private string domsTime()
 		{
-            if (StartOfRound.Instance.currentLevel.planetHasTime && StartOfRound.Instance.shipDoorsEnabled)
-                return "YesToTime";
-            return "NoToTime";
-        }
-		private string CommandFunction()
-		{
-			Logger.LogWarning("Code put here will run when terminal command is sent.");
-			return "Wait, you cannot push\n\n";
+			if (StartOfRound.Instance.currentLevel.planetHasTime && StartOfRound.Instance.shipDoorsEnabled)
+				return "YesToTime";
+			return "NoToTime";
 		}
 
         private void OnTerminalTextChanged(object sender, TerminalTextChangedEventArgs e)
@@ -128,6 +75,7 @@ namespace TerminalExpansion
 	        Logger.LogMessage("Terminal is awake");
 
 			NodeAppendLine("help", "\nSo what are we buying today?\n");
+			
 		
         }
 
