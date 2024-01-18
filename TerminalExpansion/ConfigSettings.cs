dc06a0c175bffa9f2483e57b760188df779deb65
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-01-18 13:45:30 -0500

    Clean up .idea workspace.xml
    This commit primarily removes unnecessary DLL references from the .idea workspace.xml file. These changes simplify the project structure and ease clutter within the project workspace.

---
+++ TerminalExpansion/ConfigSettings.cs
@@ -0,0 +1,125 @@
+﻿using BepInEx.Configuration;
+using JetBrains.Annotations;
+
+namespace TerminalStuff
+{
+    public static class ConfigSettings
+    {
+        //establish commands that can be turned on or off here
+        public static ConfigEntry<bool> ModNetworking;
+        public static ConfigEntry<bool> terminalLobby; //lobby name command
+        public static ConfigEntry<bool> terminalCams; //cams command
+        public static ConfigEntry<bool> terminalQuit; //quit command
+        public static ConfigEntry<bool> terminalClear; //clear command
+        public static ConfigEntry<bool> terminalLoot; //loot command
+        public static ConfigEntry<bool> terminalLol; //lol command
+        public static ConfigEntry<bool> terminalHeal; //heal command
+        public static ConfigEntry<bool> terminalFov; //Fov command
+        public static ConfigEntry<bool> terminalGamble; //Gamble command
+        public static ConfigEntry<bool> terminalLever; //Lever command
+        public static ConfigEntry<bool> terminalDanger; //Danger command
+        public static ConfigEntry<bool> terminalVitals; //Vitals command
+        public static ConfigEntry<bool> terminalBioScan; //BioScan command
+        public static ConfigEntry<bool> terminalVitalsUpgrade; //Vitals Upgrade command
+        public static ConfigEntry<bool> terminalTP; //Teleporter command
+        public static ConfigEntry<bool> terminalITP; //Inverse Teleporter command
+        public static ConfigEntry<bool> terminalMods; //Modlist command
+        public static ConfigEntry<bool> terminalKick; //Kick command (host only)
+        public static ConfigEntry<bool> terminalFcolor; //Flashlight color command
+        public static ConfigEntry<bool> terminalMap; //Map shortcut
+        public static ConfigEntry<bool> terminalMinimap; //Minimap command
+        public static ConfigEntry<bool> terminalMinicams; //Minicams command
+        public static ConfigEntry<bool> terminalOverlay; //Overlay cams command
+        public static ConfigEntry<bool> terminalDoor; //Door Toggle command
+        public static ConfigEntry<bool> terminalLights; //Light Toggle command
+        public static ConfigEntry<bool> terminalScolor; //Light colors command
+        public static ConfigEntry<bool> terminalAlwaysOn; //AlwaysOn command
+        public static ConfigEntry<bool> terminalLink; //Link command
+        public static ConfigEntry<bool> terminalLink2; //Link2 command
+        public static ConfigEntry<bool> terminalRandomSuit; //RandomSuit command
+
+
+        //Strings for display messages
+        public static ConfigEntry<string> doorOpenString; //Door String
+        public static ConfigEntry<string> doorCloseString; //Door String
+        public static ConfigEntry<string> doorSpaceString; //Door String
+        public static ConfigEntry<string> quitString; //Quit String
+        public static ConfigEntry<string> leverString; //Lever String
+        public static ConfigEntry<string> kickString; //Kick String
+        public static ConfigEntry<string> kickNoString; //Kick NO string
+        public static ConfigEntry<string> kickNotHostString; //Kick not host string
+        public static ConfigEntry<string> lolStartString; //lol, start video string
+        public static ConfigEntry<string> lolStopString; //lol, stop video string
+        public static ConfigEntry<string> tpMessageString; //TP Message String
+        public static ConfigEntry<string> itpMessageString; //TP Message String
+        public static ConfigEntry<string> vitalsPoorString; //Vitals can't afford string
+        public static ConfigEntry<string> vitalsUpgradePoor; //Vitals Upgrade can't afford string
+        public static ConfigEntry<string> healIsFullString; //full health string
+        public static ConfigEntry<string> healString; //healing player string
+        public static ConfigEntry<string> camString; //Cameras on string
+        public static ConfigEntry<string> camString2; //Cameras off string
+        public static ConfigEntry<string> mapString; //map on string
+        public static ConfigEntry<string> mapString2; //map off string
+        public static ConfigEntry<string> ovString; //overlay on string
+        public static ConfigEntry<string> ovString2; //overlay off string
+        public static ConfigEntry<string> mmString; //minimap on string
+        public static ConfigEntry<string> mmString2; //minimap off string
+        public static ConfigEntry<string> mcString; //minicam on string
+        public static ConfigEntry<string> mcString2; //minicam off string
+
+
+        //Cost configs
+        public static ConfigEntry<int> vitalsCost; //Cost of Vitals Command
+        public static ConfigEntry<int> vitalsUpgradeCost; //Cost of Vitals Upgrade Command
+        public static ConfigEntry<int> bioScanUpgradeCost; //Cost of Enemy Scan Upgrade Command
+        public static ConfigEntry<int> enemyScanCost; //Cost of Enemy Scan Command
+
+        //Other config items
+        public static ConfigEntry<int> gambleMinimum; //Minimum amount of credits needed to gamble
+        public static ConfigEntry<bool> gamblePityMode; //enable or disable pity for gamblers
+        public static ConfigEntry<int> gamblePityCredits; //Pity Credits for losers
+        public static ConfigEntry<string> gamblePoorString; //gamble credits too low string
+        public static ConfigEntry<string> videoFolderPath; //Specify a different folder with videos
+        public static ConfigEntry<bool> leverConfirmOverride; //disable confirmation check for lever
+        public static ConfigEntry<bool> camsNeverHide;
+        public static ConfigEntry<bool> networkedNodes; //enable or disable networked terminal nodes (beta)
+        public static ConfigEntry<string> defaultCamsView;
+        public static ConfigEntry<int> ovOpacity; //Opacity Percentage for Overlay Cams View
+        public static ConfigEntry<string> customLink;
+        public static ConfigEntry<string> customLink2;
+        public static ConfigEntry<string> customLinkHint;
+        public static ConfigEntry<string> customLink2Hint;
+        public static ConfigEntry<string> homeLine1;
+        public static ConfigEntry<string> homeLine2;
+        public static ConfigEntry<string> homeLine3;
+
+        //keyword strings (terminalapi)
+        public static ConfigEntry<string> alwaysOnKeyword; //string to match keyword
+        public static ConfigEntry<string> minimapKeyword;
+        public static ConfigEntry<string> minicamsKeyword;
+        public static ConfigEntry<string> overlayKeyword;
+        public static ConfigEntry<string> doorKeyword;
+        public static ConfigEntry<string> lightsKeyword;
+        public static ConfigEntry<string> modsKeyword2;
+        public static ConfigEntry<string> tpKeyword2;
+        public static ConfigEntry<string> itpKeyword2;
+        public static ConfigEntry<string> quitKeyword2;
+        public static ConfigEntry<string> lolKeyword;
+        public static ConfigEntry<string> clearKeyword2;
+        public static ConfigEntry<string> dangerKeyword;
+        public static ConfigEntry<string> healKeyword2;
+        public static ConfigEntry<string> lootKeyword2;
+        public static ConfigEntry<string> camsKeyword2;
+        public static ConfigEntry<string> mapKeyword2;
+        public static ConfigEntry<string> randomSuitKeyword;
+
+        //terminal patcher keywords
+        public static ConfigEntry<string> fcolorKeyword;
+        public static ConfigEntry<string> gambleKeyword;
+        public static ConfigEntry<string> leverKeyword;
+        public static ConfigEntry<string> scolorKeyword;
+        public static ConfigEntry<string> linkKeyword;
+        public static ConfigEntry<string> link2Keyword;
+        
+    }
+}
--- TerminalExpansion/Plugin.cs
+++ TerminalExpansion/Plugin.cs
@@ -1,12 +1,18 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using BepInEx;
 using System.Web;
 using GameNetcodeStuff;
-using LethalOS.API.Terminal;
 using TerminalApi;
 using TerminalApi.Classes;
+using TerminalStuff;
 using UnityEngine;
+using UnityEngine.Events;
 using static System.Net.Mime.MediaTypeNames;
 using static TerminalApi.Events.Events;
 using static TerminalApi.TerminalApi;
@@ -31,7 +37,7 @@ namespace TerminalExpansion
 			TerminalBeganUsing += BeganUsing;
 			TerminalExited += OnTerminalExit;
             TerminalTextChanged += OnTerminalTextChanged;
-
+            
             //Add the time command
             
             AddCommand("time", new CommandInfo()
@@ -44,6 +50,16 @@ namespace TerminalExpansion
                 Category = "Other",
                 Description = "This command will display the current time."
             });
+            AddCommand("total", new CommandInfo()
+            {
+	            DisplayTextSupplier = () =>
+	            {
+		            Logger.LogWarning("Total Loot Calculated!");
+		            return getLoot() + DoorCommand() + "\n";
+	            },
+	            Category = "Other",
+	            Description = "This command will display the current time."
+            });
             
 		}
 		public string KillPlayerCommand(PlayerControllerB player)
@@ -53,6 +69,68 @@ namespace TerminalExpansion
 
 			player.KillPlayer(Vector3.up);
 			return $"Killed {player.playerUsername}";
+		}
+
+		private string DoorCommand()
+		{
+			 if (!StartOfRound.Instance.inShipPhase)
+                        {
+                            //thanks chatgpt for the breakdown
+
+                            // Determine the button name based on the hangar doors state
+                            string buttonName = StartOfRound.Instance.hangarDoorsClosed ? "StartButton" : "StopButton";
+
+                            // Find the corresponding button GameObject
+                            GameObject buttonObject = GameObject.Find(buttonName);
+
+                            // Get the InteractTrigger component from the button
+                            InteractTrigger interactTrigger = buttonObject.GetComponentInChildren<InteractTrigger>();
+
+                            // Determine the action based on the hangar doors state
+                            string action = StartOfRound.Instance.hangarDoorsClosed ? "opened" : "closed";
+
+                            // Log the door state
+                            Logger.LogMessage($"Hangar doors are {action}.");
+
+                            // Invoke the onInteract event if the button and event are found
+                            if (interactTrigger != null)
+                            {
+                                UnityEvent<PlayerControllerB> onInteractEvent = interactTrigger.onInteract as UnityEvent<PlayerControllerB>;
+
+                                if (onInteractEvent != null)
+                                {
+                                    onInteractEvent.Invoke(GameNetworkManager.Instance.localPlayerController);
+
+                                    // Log individual messages for open and close events
+                                    if (action == "opened")
+                                    {
+                                        Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+                                        return $"{ConfigSettings.doorOpenString.Value}\n";
+                                    }
+                                    else if (action == "closed")
+                                    {
+                                        Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+                                        return $"{ConfigSettings.doorCloseString.Value}\n";
+                                    }
+                                }
+                                else
+                                {
+                                    // Log if onInteractEvent is null
+                                    Logger.LogWarning($"Warning: onInteract event is null for button {buttonName}.");
+                                }
+                            }
+                            else
+                            {
+                                // Log if interactTrigger is null
+                                Logger.LogWarning($"Warning: InteractTrigger not found on button {buttonName}.");
+                            }
+                        }
+                        else
+                        {
+                            return $"{ConfigSettings.doorSpaceString.Value}\n";
+                        }
+
+			return "";
 		}
 		
 		private string Time()
@@ -126,18 +204,18 @@ namespace TerminalExpansion
             isInUse = true;
         }
 
-        public void Init()
+        public static String getLoot()
         {
-	        var utilsMenu = new Menu("TerminalUtils", "Some terminal utilities.", "terminalutils", "Verity");
-        
-	        //EnemyCategory
-	        var enemyCategory = new Category("EnemyUtils", "Enemy utils.", "enemyutils");
-	        //enemyCategory.AddModule(new EnemyScan());
-	        //enemyCategory.AddModule(new DespawnEnemies());
-	        //EnemyCategory
-        
-	        utilsMenu.AddCategory(enemyCategory);
-	        utilsMenu.Finished();
+	        String totalvalue = string.Empty;
+	        float lootValue = CalculateLootValue();
+	        return string.Format("Total Value on Ship: ${0:F0}", (object)lootValue); 
+        }
+        public static float CalculateLootValue()
+        {
+	        List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)GameObject.Find("/Environment/HangarShip").GetComponentsInChildren<GrabbableObject>())
+		        .Where<GrabbableObject>(obj => obj.name != "ClipboardManual" && obj.name != "StickyNoteItem").ToList<GrabbableObject>();
+	        
+	        return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue);
         }
     }
 }
