4abacfa0039fa9b583446f8155b2cb1234df45ac
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-01-19 13:34:00 -0500

    Remove Commands.cs and update terminal commands in Plugin.cs
    The Commands.cs file has been removed and terminal commands formerly managed by this file have been added directly to Plugin.cs. Additionally, ConfigSettings.cs has been simplified with several strings reduced to basic functionality. Other minor modifications are to workspace.xml to reflect these changes.

--- TerminalExpansion/ConfigSettings.cs
+++ TerminalExpansion/ConfigSettings.cs
@@ -1,63 +1,12 @@
-﻿using BepInEx.Configuration;
+﻿﻿using BepInEx.Configuration;
 using JetBrains.Annotations;
 
-namespace TerminalStuff
+namespace TerminalExpansion
 {
     public static class ConfigSettings
     {
-        //Strings for display messages
-        public static ConfigEntry<string> doorOpenString; //Door String
-        public static ConfigEntry<string> doorCloseString; //Door String
-        public static ConfigEntry<string> doorSpaceString; //Door String
-        public static ConfigEntry<string> quitString; //Quit String
-        public static ConfigEntry<string> leverString; //Lever String
-        public static ConfigEntry<string> kickString; //Kick String
-        public static ConfigEntry<string> kickNoString; //Kick NO string
-        public static ConfigEntry<string> kickNotHostString; //Kick not host string
-        public static ConfigEntry<string> lolStartString; //lol, start video string
-        public static ConfigEntry<string> lolStopString; //lol, stop video string
-        public static ConfigEntry<string> tpMessageString; //TP Message String
-        public static ConfigEntry<string> itpMessageString; //TP Message String
-        public static ConfigEntry<string> vitalsPoorString; //Vitals can't afford string
-        public static ConfigEntry<string> vitalsUpgradePoor; //Vitals Upgrade can't afford string
-        public static ConfigEntry<string> healIsFullString; //full health string
-        public static ConfigEntry<string> healString; //healing player string
-        public static ConfigEntry<string> camString; //Cameras on string
-        public static ConfigEntry<string> camString2; //Cameras off string
-        public static ConfigEntry<string> mapString; //map on string
-        public static ConfigEntry<string> mapString2; //map off string
-        public static ConfigEntry<string> ovString; //overlay on string
-        public static ConfigEntry<string> ovString2; //overlay off string
-        public static ConfigEntry<string> mmString; //minimap on string
-        public static ConfigEntry<string> mmString2; //minimap off string
-        public static ConfigEntry<string> mcString; //minicam on string
-        public static ConfigEntry<string> mcString2; //minicam off string
-
-
-        //Cost configs
-        public static ConfigEntry<int> vitalsCost; //Cost of Vitals Command
-        public static ConfigEntry<int> vitalsUpgradeCost; //Cost of Vitals Upgrade Command
-        public static ConfigEntry<int> bioScanUpgradeCost; //Cost of Enemy Scan Upgrade Command
-        public static ConfigEntry<int> enemyScanCost; //Cost of Enemy Scan Command
-
-        //Other config items
-        public static ConfigEntry<int> gambleMinimum; //Minimum amount of credits needed to gamble
-        public static ConfigEntry<bool> gamblePityMode; //enable or disable pity for gamblers
-        public static ConfigEntry<int> gamblePityCredits; //Pity Credits for losers
-        public static ConfigEntry<string> gamblePoorString; //gamble credits too low string
-        public static ConfigEntry<string> videoFolderPath; //Specify a different folder with videos
-        public static ConfigEntry<bool> leverConfirmOverride; //disable confirmation check for lever
-        public static ConfigEntry<bool> camsNeverHide;
-        public static ConfigEntry<bool> networkedNodes; //enable or disable networked terminal nodes (beta)
-        public static ConfigEntry<string> defaultCamsView;
-        public static ConfigEntry<int> ovOpacity; //Opacity Percentage for Overlay Cams View
-        public static ConfigEntry<string> customLink;
-        public static ConfigEntry<string> customLink2;
-        public static ConfigEntry<string> customLinkHint;
-        public static ConfigEntry<string> customLink2Hint;
-        public static ConfigEntry<string> homeLine1;
-        public static ConfigEntry<string> homeLine2;
-        public static ConfigEntry<string> homeLine3;
-        
+        public static string doorSpaceString = "You're in space R-tard";
+        public static string doorOpenString = "The door is open!";
+        public static string doorCloseString = "The door is closed!";
     }
 }
--- TerminalExpansion/Plugin.cs
+++ TerminalExpansion/Plugin.cs
@@ -1,9 +1,14 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using BepInEx;
 using GameNetcodeStuff;
 using TerminalApi.Classes;
 using TerminalApi.Events;
 using TimeTerminalCommand;
 using UnityEngine;
+using UnityEngine.Events;
 using static TerminalApi.Events.Events;
 using static TerminalApi.TerminalApi;
 
@@ -15,7 +20,6 @@ namespace TerminalExpansion
 	{
 		private bool isInUse;
 
-		private Commands _commandMgr = new Commands();
 		private void Awake()
 		{
 			Logger.LogInfo("Plugin Test Plugin is loaded!");
@@ -28,32 +32,46 @@ namespace TerminalExpansion
 			TerminalBeginUsing += OnBeginUsing;
 			TerminalBeganUsing += BeganUsing;
 			TerminalExited += OnTerminalExit;
-            TerminalTextChanged += OnTerminalTextChanged;
-            
-            //Add the time command
-            
-            AddCommand("time", new CommandInfo()
-            {
-                DisplayTextSupplier = () =>
-                {
-                    Logger.LogWarning("Time command ran!");
-					return _commandMgr.Time() + "\n";
-                },
-                Category = "Other",
-                Description = "This command will display the current time."
-            });
-            AddCommand("total", new CommandInfo()
-            {
-	            DisplayTextSupplier = () =>
-	            {
-		            Logger.LogWarning("Total Loot Calculated!");
-		            return _commandMgr.getLoot() + _commandMgr.DoorCommand() + "\n";
-	            },
-	            Category = "Other",
-	            Description = "This command will display the current time."
-            });
-            
+			TerminalTextChanged += OnTerminalTextChanged;
+			
+			// Now that _commandMgr is initialized, add your commands
+			AddCommand("time", new CommandInfo()
+			{
+				DisplayTextSupplier = () =>
+				{
+					Logger.LogWarning("Time command ran!");
+					return Time() + "\n";
+				},
+				Category = "Other",
+				Description = "This command will display the current time."
+			});
+
+			AddCommand("total", new CommandInfo()
+			{
+				DisplayTextSupplier = () =>
+				{
+					Logger.LogWarning("Total Loot Calculated!");
+					return getLoot() + "\n";
+				},
+				Category = "Other",
+				Description = "This command will calculate the total loot on the ship"
+			});
+			AddCommand("door", new CommandInfo()
+			{
+				DisplayTextSupplier = () =>
+				{
+					Logger.LogWarning("Door Command Ran");
+					TerminalKeyword TempWord = new TerminalKeyword();
+					TempWord.isVerb = true;
+					TempWord.word = "toggle";
+					AddTerminalKeyword(TempWord);
+					return DoorCommand() + "\n";
+				},
+				Category = "Other",
+				Description = "This command will toggle the hanger door"
+			},"toggle", false);
 		}
+
 		public string KillPlayerCommand(PlayerControllerB player)
 		{
 			if (StartOfRound.Instance == null)
@@ -62,6 +80,76 @@ namespace TerminalExpansion
 			player.KillPlayer(Vector3.up);
 			return $"Killed {player.playerUsername}";
 		}
+		
+		public string DoorCommand()
+		{
+			if (!StartOfRound.Instance.inShipPhase)
+			{
+				// Determine the button name based on the hangar doors state
+				string buttonName = StartOfRound.Instance.hangarDoorsClosed ? "StartButton" : "StopButton";
+
+				// Find the corresponding button GameObject
+				GameObject buttonObject = GameObject.Find(buttonName);
+
+				// Get the InteractTrigger component from the button
+				InteractTrigger interactTrigger = buttonObject?.GetComponentInChildren<InteractTrigger>();
+
+				// Determine the action based on the hangar doors state
+				string action = StartOfRound.Instance.hangarDoorsClosed ? "opened" : "closed";
+
+				// Log the door state
+				Debug.Log($"Hangar doors are {action}.");
+
+				// Invoke the onInteract event if the button and event are found
+				UnityEvent<PlayerControllerB> onInteractEvent = interactTrigger?.onInteract as UnityEvent<PlayerControllerB>;
+
+				// Invoke the event only if all components are non-null
+				onInteractEvent?.Invoke(GameNetworkManager.Instance?.localPlayerController);
+
+				// Log individual messages for open and close events
+				if (action == "opened")
+				{
+					Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+					return $"{ConfigSettings.doorOpenString}\n";
+				}
+				else if (action == "closed")
+				{
+					Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+					return $"{ConfigSettings.doorCloseString}\n";
+				}
+			}
+			else
+			{
+				return $"{ConfigSettings.doorSpaceString}\n";
+			}
+			return "";
+		}
+
+		public string Time()
+		{
+			if (StartOfRound.Instance.currentLevel.planetHasTime &&
+			    StartOfRound.Instance.shipDoorsEnabled)
+			{
+				return "Current time: " +
+				       HUDManager.Instance.clockNumber.text.Replace('\n', ' ');
+			}
+			return "Time isn't Available";
+		}
+        public string getLoot()
+        {
+	        string totalvalue = string.Empty;
+	        float lootValue = CalculateLootValue();
+	        return string.Format("Total Value on Ship: ${0:F0}", (object)lootValue); 
+        }
+        public float CalculateLootValue()
+        {
+	        List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)GameObject.Find("/Environment/HangarShip").GetComponentsInChildren<GrabbableObject>())
+		        .Where<GrabbableObject>(obj => obj.name != "ClipboardManual" && obj.name != "StickyNoteItem").ToList<GrabbableObject>();
+	        
+	        return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue);
+        }
+		
+		
 		private void OnTerminalTextChanged(object sender, Events.TerminalTextChangedEventArgs e)
 		{
 			string userInput = GetTerminalInput();
@@ -71,9 +159,7 @@ namespace TerminalExpansion
 			{
 				SetTerminalInput("How do you know my name?");
 			}
-			
 		}
-        
 
 		private void OnTerminalExit(object sender, Events.TerminalEventArgs e)
 		{
@@ -86,9 +172,7 @@ namespace TerminalExpansion
 			Logger.LogMessage("Terminal is awake");
 
 			NodeAppendLine("help", "So what are we buying today?\n" +
-			                       "Or are we going to Titan?\n");
-			
-		
+			                       "Or are we going to Rend?\n");
 		}
 
 		private void TerminalIsWaking(object sender, Events.TerminalEventArgs e)
@@ -120,16 +204,6 @@ namespace TerminalExpansion
 		{
 			Logger.LogMessage("Player is using terminal");
 			isInUse = true;
-		}
-
-		public void LogMessage(string str)
-		{
-			Logger.LogMessage(str);
-		}
-
-		public void LogWarning(string str)
-		{
-			Logger.LogWarning(str);
 		}
     }
 }
