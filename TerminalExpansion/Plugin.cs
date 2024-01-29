6bae539bfd6d43d8b6e9394f465bb00833528059
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-01-29 13:16:29 -0500

    Update terminal commands and remove Commands.cs
    Commands.cs has been removed and terminal commands have been migrated directly to Plugin.cs. This includes the addition of three new in-game commands: `time`, `total`, and `door`. Interactivity of these commands is enhanced with lambda expression threading and properly formatted terminal returns. Other modifications are also made in workspace.xml to reflect these changes.

--- TerminalExpansion/Plugin.cs
+++ TerminalExpansion/Plugin.cs
@@ -24,6 +24,7 @@ namespace TerminalExpansion
 		{
 			Logger.LogInfo("Plugin Test Plugin is loaded!");
 
+			// Loads the states the terminal can be in during the game
 			TerminalAwake +=  TerminalIsAwake;
 			TerminalWaking += TerminalIsWaking;
 			TerminalStarting += TerminalIsStarting;
@@ -34,10 +35,11 @@ namespace TerminalExpansion
 			TerminalExited += OnTerminalExit;
 			TerminalTextChanged += OnTerminalTextChanged;
 			
-			// Now that _commandMgr is initialized, add your commands
+			// Adds the time command to the game with the CommandInfo Class to show
 			AddCommand("time", new CommandInfo()
 			{
-				DisplayTextSupplier = () =>
+				DisplayTextSupplier = () => // Using Lambda Expression to run
+                                // these functions in different threads
 				{
 					Logger.LogWarning("Time command ran!");
 					return Time() + "\n";
@@ -46,9 +48,11 @@ namespace TerminalExpansion
 				Description = "This command will display the current time."
 			});
 
+			// Adds the total command to the terminal along with a CommandInfo class
 			AddCommand("total", new CommandInfo()
 			{
-				DisplayTextSupplier = () =>
+				DisplayTextSupplier = () => // Using Lambda Expression to run
+                                // these functions in different threads
 				{
 					Logger.LogWarning("Total Loot Calculated!");
 					return getLoot() + "\n";
@@ -56,13 +60,19 @@ namespace TerminalExpansion
 				Category = "Other",
 				Description = "This command will calculate the total loot on the ship"
 			});
+			
+			// Adds the door control command to the
+			// game along with a CommandInfo Class
 			AddCommand("door", new CommandInfo()
 			{
-				DisplayTextSupplier = () =>
+				DisplayTextSupplier = () => // Using Lambda Expression to run
+                                // these functions in different threads
 				{
 					Logger.LogWarning("Door Command Ran");
-					TerminalKeyword TempWord = new TerminalKeyword();
-					TempWord.isVerb = true;
+					TerminalKeyword TempWord = new TerminalKeyword(); 
+					// Makes a new terminal keyword to allow the
+					// command to be executed under different names
+					TempWord.isVerb = true; 
 					TempWord.word = "toggle";
 					AddTerminalKeyword(TempWord);
 					return DoorCommand() + "\n";
@@ -71,50 +81,49 @@ namespace TerminalExpansion
 				Description = "This command will toggle the hanger door"
 			},"toggle", false);
 		}
-
-		public string KillPlayerCommand(PlayerControllerB player)
-		{
-			if (StartOfRound.Instance == null)
-				return "Game not started";
-
-			player.KillPlayer(Vector3.up);
-			return $"Killed {player.playerUsername}";
-		}
 		
 		public string DoorCommand()
 		{
+			// Check if the ship is in Orbit or not
 			if (!StartOfRound.Instance.inShipPhase)
 			{
 				// Determine the button name based on the hangar doors state
-				string buttonName = StartOfRound.Instance.hangarDoorsClosed ? "StartButton" : "StopButton";
+				string buttonName = StartOfRound.Instance.hangarDoorsClosed
+					? "StartButton" : "StopButton";
 
 				// Find the corresponding button GameObject
 				GameObject buttonObject = GameObject.Find(buttonName);
 
 				// Get the InteractTrigger component from the button
-				InteractTrigger interactTrigger = buttonObject?.GetComponentInChildren<InteractTrigger>();
+				InteractTrigger interactTrigger = 
+					buttonObject?.GetComponentInChildren<InteractTrigger>();
 
 				// Determine the action based on the hangar doors state
-				string action = StartOfRound.Instance.hangarDoorsClosed ? "opened" : "closed";
+				string action = StartOfRound.Instance.hangarDoorsClosed 
+					? "opened" : "closed";
 
 				// Log the door state
 				Debug.Log($"Hangar doors are {action}.");
 
 				// Invoke the onInteract event if the button and event are found
-				UnityEvent<PlayerControllerB> onInteractEvent = interactTrigger?.onInteract as UnityEvent<PlayerControllerB>;
+				UnityEvent<PlayerControllerB> onInteractEvent 
+					= interactTrigger?.onInteract as UnityEvent<PlayerControllerB>;
 
 				// Invoke the event only if all components are non-null
-				onInteractEvent?.Invoke(GameNetworkManager.Instance?.localPlayerController);
+				onInteractEvent?.Invoke
+					(GameNetworkManager.Instance?.localPlayerController);
 
 				// Log individual messages for open and close events
 				if (action == "opened")
 				{
-					Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+					Logger.LogMessage($"Hangar doors {action} successfully" + 
+					             $" by interacting with button {buttonName}.");
 					return $"{ConfigSettings.doorOpenString}\n";
 				}
 				else if (action == "closed")
 				{
-					Logger.LogMessage($"Hangar doors {action} successfully by interacting with button {buttonName}.");
+					Logger.LogMessage($"Hangar doors {action} successfully" +
+					             $" by interacting with button {buttonName}.");
 					return $"{ConfigSettings.doorCloseString}\n";
 				}
 			}
@@ -133,32 +142,46 @@ namespace TerminalExpansion
 				return "Current time: " +
 				       HUDManager.Instance.clockNumber.text.Replace('\n', ' ');
 			}
-			return "Time isn't Available";
+			return "In space time doesn't exist for some reason";
 		}
+		// This will run the CalculateLootValue and return it to the terminal
         public string getLoot()
         {
 	        string totalvalue = string.Empty;
 	        float lootValue = CalculateLootValue();
 	        return string.Format("Total Value on Ship: ${0:F0}", (object)lootValue); 
+	        //Returns the total value correctly formatted
         }
         public float CalculateLootValue()
         {
-	        List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)GameObject.Find("/Environment/HangarShip").GetComponentsInChildren<GrabbableObject>())
-		        .Where<GrabbableObject>(obj => obj.name != "ClipboardManual" && obj.name != "StickyNoteItem").ToList<GrabbableObject>();
+	        // This looks like a lot but basically it grabs all GrabbaleObjects
+	        // in the HangerShip while ignoring two items on the ship that don't add value
+	        List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)
+			        GameObject.Find("/Environment/HangarShip")
+				    .GetComponentsInChildren<GrabbableObject>())
+					.Where<GrabbableObject>(obj => obj.name
+			        != "ClipboardManual" &&
+			        obj.name != "StickyNoteItem").ToList<GrabbableObject>(); 
 	        
-	        return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue);
+	        return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue); 
+	        // Gets the sum while formatting 
         }
 		
-		
-		private void OnTerminalTextChanged(object sender, Events.TerminalTextChangedEventArgs e)
+		// This will run every time a new character is typed into the terminal
+		private void OnTerminalTextChanged(object sender,
+			Events.TerminalTextChangedEventArgs e)
 		{
 			string userInput = GetTerminalInput();
 			Logger.LogMessage(userInput);
 
-			if(userInput.Equals("Dominic"))
+			switch (userInput.ToLower())
 			{
-				SetTerminalInput("How do you know my name?");
+				case "dominic":
+					SetTerminalInput("How do you know my name?");
+					break;
 			}
+
+			
 		}
 
 		private void OnTerminalExit(object sender, Events.TerminalEventArgs e)
@@ -190,9 +213,11 @@ namespace TerminalExpansion
 			Logger.LogMessage("Terminal is started");
 		}
 
-		private void TextSubmitted(object sender, Events.TerminalParseSentenceEventArgs e)
+		private void TextSubmitted(object sender,
+			Events.TerminalParseSentenceEventArgs e)
 		{
-			Logger.LogMessage($"Text submitted: {e.SubmittedText} Node Returned: {e.ReturnedNode}");
+			Logger.LogMessage($"Text submitted:" +
+			                  $" {e.SubmittedText} Node Returned: {e.ReturnedNode}");
 		}
 
 		private void OnBeginUsing(object sender, Events.TerminalEventArgs e)
