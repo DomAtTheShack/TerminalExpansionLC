2e669ac062d8772f89f03ea70a8a2118f2768028
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-01-30 12:57:38 -0500

    Update terminal commands and fix bugs in Plugin.cs
    This commit primarily updates functionalities and commands in Plugin.cs. Significant changes include the reformat of several lines of code for better readability and correction of methods' names that mirrored internal helper functions. The commit also introduces a new "suit" command, which allows the terminal to assign a random suit to a player. Several bugs in the terminal use lifecycle were addressed, mainly issues with the terminal use status flag.

--- TerminalExpansion/Plugin.cs
+++ TerminalExpansion/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using BepInEx;
 using GameNetcodeStuff;
@@ -22,7 +23,7 @@ namespace TerminalExpansion
 
 		private void Awake()
 		{
-			Logger.LogInfo("Plugin Test Plugin is loaded!");
+			Logger.LogInfo("TerminalExpansion Plugin is loaded!");
 
 			// Loads the states the terminal can be in during the game
 			TerminalAwake +=  TerminalIsAwake;
@@ -30,7 +31,7 @@ namespace TerminalExpansion
 			TerminalStarting += TerminalIsStarting;
 			TerminalStarted += TerminalIsStarted;
 			TerminalParsedSentence += TextSubmitted;
-			TerminalBeginUsing += OnBeginUsing;
+			TerminalBeginUsing += OnUse;
 			TerminalBeganUsing += BeganUsing;
 			TerminalExited += OnTerminalExit;
 			TerminalTextChanged += OnTerminalTextChanged;
@@ -42,7 +43,7 @@ namespace TerminalExpansion
                                 // these functions in different threads
 				{
 					Logger.LogWarning("Time command ran!");
-					return Time() + "\n";
+					return GetCurrentTime() + "\n";
 				},
 				Category = "Other",
 				Description = "This command will display the current time."
@@ -55,7 +56,7 @@ namespace TerminalExpansion
                                 // these functions in different threads
 				{
 					Logger.LogWarning("Total Loot Calculated!");
-					return getLoot() + "\n";
+					return GetLootFormatted() + "\n";
 				},
 				Category = "Other",
 				Description = "This command will calculate the total loot on the ship"
@@ -75,14 +76,25 @@ namespace TerminalExpansion
 					TempWord.isVerb = true; 
 					TempWord.word = "toggle";
 					AddTerminalKeyword(TempWord);
-					return DoorCommand() + "\n";
+					return ToggleDoor() + "\n";
 				},
 				Category = "Other",
 				Description = "This command will toggle the hanger door"
 			},"toggle", false);
+			
+			AddCommand("suit", new CommandInfo()
+			{
+				DisplayTextSupplier = () =>
+				{
+					Logger.LogMessage($"Getting random suit for player {GetMyPlayerID()}");
+					return getRandomSuit() + '\n';
+				},
+				Category = "Other",
+				Description = "This will change the players suit to a new random one"
+			},"random", false);
 		}
 		
-		public string DoorCommand()
+		public string ToggleDoor()
 		{
 			// Check if the ship is in Orbit or not
 			if (!StartOfRound.Instance.inShipPhase)
@@ -95,34 +107,34 @@ namespace TerminalExpansion
 				GameObject buttonObject = GameObject.Find(buttonName);
 
 				// Get the InteractTrigger component from the button
-				InteractTrigger interactTrigger = 
+				InteractTrigger interactDoorTrigger = 
 					buttonObject?.GetComponentInChildren<InteractTrigger>();
 
 				// Determine the action based on the hangar doors state
-				string action = StartOfRound.Instance.hangarDoorsClosed 
+				string doorAction = StartOfRound.Instance.hangarDoorsClosed 
 					? "opened" : "closed";
 
 				// Log the door state
-				Debug.Log($"Hangar doors are {action}.");
+				Debug.Log($"Hangar doors are {doorAction}.");
 
 				// Invoke the onInteract event if the button and event are found
 				UnityEvent<PlayerControllerB> onInteractEvent 
-					= interactTrigger?.onInteract as UnityEvent<PlayerControllerB>;
+					= interactDoorTrigger?.onInteract as UnityEvent<PlayerControllerB>;
 
 				// Invoke the event only if all components are non-null
 				onInteractEvent?.Invoke
 					(GameNetworkManager.Instance?.localPlayerController);
 
 				// Log individual messages for open and close events
-				if (action == "opened")
+				if (doorAction == "opened")
 				{
-					Logger.LogMessage($"Hangar doors {action} successfully" + 
+					Logger.LogMessage($"Hangar doors {doorAction} successfully" + 
 					             $" by interacting with button {buttonName}.");
 					return $"{ConfigSettings.doorOpenString}\n";
 				}
-				else if (action == "closed")
+				else if (doorAction == "closed")
 				{
-					Logger.LogMessage($"Hangar doors {action} successfully" +
+					Logger.LogMessage($"Hangar doors {doorAction} successfully" +
 					             $" by interacting with button {buttonName}.");
 					return $"{ConfigSettings.doorCloseString}\n";
 				}
@@ -133,8 +145,112 @@ namespace TerminalExpansion
 			}
 			return "";
 		}
+		
+		private void BeganUsing(object sender, Events.TerminalEventArgs e)
+		{
+			Logger.LogMessage("Player is using terminal");
+			isInUse = true;
+		}
+		
+		public float CalculateLootValue()
+		{
+			// This looks like a lot but basically it grabs all GrabbaleObjects
+			// in the HangerShip while ignoring two items on the ship that don't add value
+			List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)
+					GameObject.Find("/Environment/HangarShip")
+						.GetComponentsInChildren<GrabbableObject>())
+				.Where<GrabbableObject>(obj => obj.name
+				                               != "ClipboardManual" &&
+				                               obj.name != "StickyNoteItem").ToList<GrabbableObject>(); 
+	        
+			return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue); 
+			// Gets the sum while formatting 
+		}
 
-		public string Time()
+		private string getRandomSuit()
+            {
+                List<UnlockableSuit> allSuits = new List<UnlockableSuit>();
+                List<UnlockableItem> UnlockableSuits = new List<UnlockableItem>();
+
+                //get allSuits
+                allSuits = Resources.FindObjectsOfTypeAll<UnlockableSuit>().ToList();
+                string displayText = string.Empty;
+
+                if (allSuits.Count > 1)
+                {
+                    // Order the list by syncedSuitID.Value
+                    allSuits = allSuits.OrderBy((UnlockableSuit suit) => suit.suitID).ToList();
+
+                    allSuits.RemoveAll(suit => suit.syncedSuitID.Value < 0); //simply remove bad suit IDs
+
+                    UnlockableSuits = StartOfRound.Instance.unlockablesList.unlockables;
+
+                    
+                    int playerID = GetMyPlayerID();
+
+
+                    if (UnlockableSuits != null)
+                    {
+                        for (int i = 0; i < UnlockableSuits.Count; i++)
+                        {
+                            // Get a random index
+                            int randomIndex = UnityEngine.Random.Range(0, allSuits.Count);
+                            string SuitName;
+
+                            // Get the UnlockableSuit at the random index
+                            UnlockableSuit randomSuit = allSuits[randomIndex];
+                            if (randomSuit != null && UnlockableSuits[randomSuit.syncedSuitID.Value] != null)
+                            {
+                                SuitName = UnlockableSuits[randomSuit.syncedSuitID.Value].unlockableName;
+                                UnlockableSuit.SwitchSuitForPlayer(StartOfRound.Instance.allPlayerScripts[playerID], randomSuit.syncedSuitID.Value, true);
+                                randomSuit.SwitchSuitServerRpc(playerID);
+                                randomSuit.SwitchSuitClientRpc(playerID);
+                                displayText = $"Changing suit to {SuitName}!\r\n";
+                                return displayText;
+                            }
+                            else
+                            {
+                                displayText = "A suit could not be found.\r\n";
+                                Logger.LogInfo($"Random suit ID was invalid or null");
+                                return displayText;
+                            }
+                        }
+                    }
+
+                    displayText = "A suit could not be found.\r\n";
+                    Logger.LogMessage($"Unlockables are null");
+                    return displayText;
+                }
+                else
+                {
+                    displayText = "Not enough suits detected.\r\n";
+                    Logger.LogMessage($"allsuits count too low");
+                    return displayText;
+                }
+            }
+		
+		private int GetMyPlayerID()
+		{
+			List<PlayerControllerB> allPlayers = new List<PlayerControllerB>();
+			string myName = GameNetworkManager.Instance.localPlayerController.playerUsername;
+			int returnID = -1;
+			allPlayers = StartOfRound.Instance.allPlayerScripts.ToList();
+			allPlayers = allPlayers.OrderBy((PlayerControllerB player) => player.playerClientId).ToList();
+			for (int i = 0; i < allPlayers.Count; i++)
+			{
+				if (StartOfRound.Instance.allPlayerScripts[i].playerUsername == myName)
+				{
+					Logger.LogInfo("Found my playerID");
+					returnID = i;
+					break;
+				}
+			}
+			if (returnID == -1)
+				Logger.LogInfo("Failed to find ID");
+			return returnID;
+		}
+		
+		public string GetCurrentTime()
 		{
 			if (StartOfRound.Instance.currentLevel.planetHasTime &&
 			    StartOfRound.Instance.shipDoorsEnabled)
@@ -144,50 +260,42 @@ namespace TerminalExpansion
 			}
 			return "In space time doesn't exist for some reason";
 		}
+		
+		
 		// This will run the CalculateLootValue and return it to the terminal
-        public string getLoot()
+        public string GetLootFormatted()
         {
-	        string totalvalue = string.Empty;
 	        float lootValue = CalculateLootValue();
 	        return string.Format("Total Value on Ship: ${0:F0}", (object)lootValue); 
 	        //Returns the total value correctly formatted
         }
-        public float CalculateLootValue()
-        {
-	        // This looks like a lot but basically it grabs all GrabbaleObjects
-	        // in the HangerShip while ignoring two items on the ship that don't add value
-	        List<GrabbableObject> list = ((IEnumerable<GrabbableObject>)
-			        GameObject.Find("/Environment/HangarShip")
-				    .GetComponentsInChildren<GrabbableObject>())
-					.Where<GrabbableObject>(obj => obj.name
-			        != "ClipboardManual" &&
-			        obj.name != "StickyNoteItem").ToList<GrabbableObject>(); 
-	        
-	        return (float)list.Sum<GrabbableObject>(scrap => scrap.scrapValue); 
-	        // Gets the sum while formatting 
-        }
+        
+        
 		
 		// This will run every time a new character is typed into the terminal
 		private void OnTerminalTextChanged(object sender,
 			Events.TerminalTextChangedEventArgs e)
 		{
-			string userInput = GetTerminalInput();
-			Logger.LogMessage(userInput);
+			string terminalUserInput = GetTerminalInput();
+			Logger.LogMessage(terminalUserInput);
 
-			switch (userInput.ToLower())
+			switch (terminalUserInput.ToLower())
 			{
 				case "dominic":
 					SetTerminalInput("How do you know my name?");
 					break;
 			}
-
 			
+		}
+		private void OnUse(object sender, Events.TerminalEventArgs e)
+		{
+			Logger.LogMessage("Player has just started using the terminal");
 		}
 
 		private void OnTerminalExit(object sender, Events.TerminalEventArgs e)
 		{
 			Logger.LogMessage("Terminal Exited");
-			isInUse = true;
+			isInUse = false;
 		}
 
 		private void TerminalIsAwake(object sender, Events.TerminalEventArgs e)
@@ -218,17 +326,6 @@ namespace TerminalExpansion
 		{
 			Logger.LogMessage($"Text submitted:" +
 			                  $" {e.SubmittedText} Node Returned: {e.ReturnedNode}");
-		}
-
-		private void OnBeginUsing(object sender, Events.TerminalEventArgs e)
-		{
-			Logger.LogMessage("Player has just started using the terminal");
-		}
-
-		private void BeganUsing(object sender, Events.TerminalEventArgs e)
-		{
-			Logger.LogMessage("Player is using terminal");
-			isInUse = true;
 		}
     }
 }
