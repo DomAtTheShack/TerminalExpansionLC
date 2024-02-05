ecc0daa424eb879a86ccb7aba0bd99786dd7cbb5
DomAtTheShack <126879915+DomAtTheShack@users.noreply.github.com>
2024-02-05 13:10:15 -0500

    Update ConfigSettings and workspace tracking
    The changes involve an update in the message in the ConfigSettings.cs file. Additionally, updates were made to the workspace.xml file for better tracking of changes specifically changes to ConfigSettings.cs file.

--- TerminalExpansion/ConfigSettings.cs
+++ TerminalExpansion/ConfigSettings.cs
@@ -5,7 +5,7 @@ namespace TerminalExpansion
 {
     public static class ConfigSettings
     {
-        public static string doorSpaceString = "You're in space R-tard";
+        public static string doorSpaceString = "You're in space!";
         public static string doorOpenString = "The door is open!";
         public static string doorCloseString = "The door is closed!";
     }
